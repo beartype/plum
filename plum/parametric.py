f0d0a344feaf1370869e461be694c260d476a00d
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2018-05-31 13:50:24 +0100

    Fix parametric classes with arguments

--- plum/parametric.py
+++ plum/parametric.py
@@ -17,19 +17,20 @@ def parametric(Class):
                            'new-style class.')
 
     class ParametricClass(Class):
-        def __new__(cls, p):
-            if p not in subclasses:
-                def __init__(self, *args, **kw_args):
-                    Class.__init__(self, *args, **kw_args)
+        def __new__(cls, *ps):
+            try:
+                hash(ps)
+            except TypeError:
+                raise TypeError('Type parameters must be hashable.')
 
+            if ps not in subclasses:
                 def __new__(cls, *args, **kw_args):
-                    return super(Class, cls).__new__(cls, *args, **kw_args)
+                    return Class.__new__(cls)
 
-                name = Class.__name__ + '{' + str(p) + '}'
-                subclasses[p] = type(name,
-                                     (ParametricClass,),
-                                     {'__init__': __init__,
-                                      '__new__': __new__})
-            return subclasses[p]
+                name = Class.__name__ + '{' + ','.join(str(p) for p in ps) + '}'
+                subclasses[ps] = type(name,
+                                      (ParametricClass,),
+                                      {'__new__': __new__})
+            return subclasses[ps]
 
     return ParametricClass
--- tests/test_parametric.py
+++ tests/test_parametric.py
@@ -31,3 +31,18 @@ def test():
     yield ok, issubclass(type(a1), A)
     yield ok, issubclass(type(a1), Base1)
     yield ok, not issubclass(type(a1), Base2)
+
+    # Test multiple type parameters and hashability.
+    yield ok, A(1, 2) == A(1, 2)
+    yield raises, TypeError, lambda: A({})
+
+
+def test_argument():
+    @parametric
+    class A:
+        def __init__(self, x):
+            self.x = x
+
+    a = A(1)(5.)
+
+    yield eq, a.x, 5.
