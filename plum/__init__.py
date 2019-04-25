79ce5a3c51ac7dcbd8b31c6986d1af2e939743e8
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-04-25 17:07:32 +0300

    Add check for Python 3

--- plum/__init__.py
+++ plum/__init__.py
@@ -2,6 +2,13 @@
 
 from __future__ import absolute_import, division, print_function
 
+import platform
+
+version = tuple(int(x) for x in platform.python_version_tuple())
+if version[0] == 3 and version[1] < 6:
+    raise SystemError('For Python 3, Plum requires at least Python 3.6.'
+                      '')  # pragma: no cover
+
 from .dispatcher import *
 from .function import *
 from .tuple import *
