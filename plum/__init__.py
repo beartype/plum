a2d529c22c8175773452968385dac11279c092b4
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-04-25 17:37:05 +0300

    Improve formatting

--- plum/__init__.py
+++ plum/__init__.py
@@ -5,9 +5,8 @@ from __future__ import absolute_import, division, print_function
 import platform
 
 version = tuple(int(x) for x in platform.python_version_tuple())
-if version[0] == 3 and version[1] < 6:
-    raise SystemError('For Python 3, Plum requires at least Python 3.6.'
-                      '')  # pragma: no cover
+if version[0] == 3 and version[1] < 6:  # pragma: no cover
+    raise SystemError('For Python 3, Plum requires at least Python 3.6.')
 
 from .dispatcher import *
 from .function import *
