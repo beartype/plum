8d67ac61d3e3bd8f3ec6d27509cb09666f47299d
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-04-25 16:47:38 +0900

    Explicitly create dispatch and reorganise

--- tests/test_precedence.py
+++ tests/test_precedence.py
@@ -2,8 +2,10 @@
 
 from __future__ import absolute_import, division, print_function
 
-from . import dispatch, AmbiguousLookupError
+from . import Dispatcher, AmbiguousLookupError
 from . import eq, raises
+
+_dispatch = Dispatcher()
 
 
 class Element(object):
@@ -18,25 +20,25 @@ class SpecialisedElement(Element):
     pass
 
 
-@dispatch(ZeroElement, Element)
+@_dispatch(ZeroElement, Element)
 def mul(a, b):
     # Return zero.
     return a
 
 
-@dispatch(Element, SpecialisedElement)
+@_dispatch(Element, SpecialisedElement)
 def mul(a, b):
     # Perform a specialised operation.
     return b
 
 
-@dispatch(ZeroElement, Element, precedence=1)
+@_dispatch(ZeroElement, Element, precedence=1)
 def mul_precedence(a, b):
     # Return zero.
     return a
 
 
-@dispatch(Element, SpecialisedElement)
+@_dispatch(Element, SpecialisedElement)
 def mul_precedence(a, b):
     # Perform a specialised operation.
     return b
@@ -54,3 +56,43 @@ def test_precedence():
     yield eq, type(mul_precedence(zero, el)), ZeroElement
     yield eq, type(mul_precedence(el, spel)), SpecialisedElement
     yield eq, type(mul_precedence(zero, spel)), ZeroElement
+
+
+def test_extension():
+    dispatch = Dispatcher()
+
+    @dispatch()
+    def g(x):
+        return 'fallback'
+
+    @g.extend({int, str}, precedence=1)
+    def g(x):
+        return 'int or str'
+
+    @g.extend({int, float}, precedence=2)
+    def g(x):
+        return 'int or float'
+
+    yield eq, g('1'), 'int or str'
+    yield eq, g(1.0), 'int or float'
+    yield eq, g(1), 'int or float'
+
+
+def test_multi():
+    dispatch = Dispatcher()
+
+    @dispatch()
+    def g():
+        return 'fallback'
+
+    @dispatch.multi(({int, str},), ({object, str},), precedence=1)
+    def g(x):
+        return 'int or str, or object or str'
+
+    @dispatch.multi(({int, float},), (float,), precedence=2)
+    def g(x):
+        return 'int or float, or float'
+
+    yield eq, g('1'), 'int or str, or object or str'
+    yield eq, g(1.0), 'int or float, or float'
+    yield eq, g(1), 'int or float, or float'
