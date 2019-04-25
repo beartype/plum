4645e80f6c2a575fa265899fdf91814b78c56eec
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-04-25 18:28:25 +0300

    Add tests for tuple

--- tests/test_tuple.py
+++ tests/test_tuple.py
@@ -2,15 +2,13 @@
 
 from __future__ import absolute_import, division, print_function
 
-from . import Tuple as Tu
-from . import eq, neq, lt, le, ge, gt, raises, call
+from . import Tuple as Tu, Type
+from . import ok, eq, neq, lt, le, ge, gt, raises, call
 
 
-def test_corner_cases():
+def test_instantiation():
     yield raises, TypeError, lambda: Tu([int], int)
     yield raises, TypeError, lambda: Tu([int, str])
-    yield raises, RuntimeError, lambda: Tu(1)
-    yield raises, RuntimeError, lambda: Tu(int).varargs_type
 
 
 class Num(object):
@@ -25,7 +23,20 @@ class FP(Re):
     pass
 
 
-def test_tuple():
+def test_properties():
+    yield eq, repr(Tu(Num)), '({!r})'.format(Type(Num))
+    yield eq, hash(Tu(Num)), hash(Tu(Num))
+    yield neq, hash(Tu(Num)), hash(Type(Num))
+    yield eq, len(Tu(Num)), 1
+    yield eq, len(Tu(Num, [Num])), 1
+
+
+def test_comparability():
+    yield ok, Tu(Num).is_comparable(Tu(FP))
+    yield ok, not Tu(Num).is_comparable(Tu(int))
+
+
+def test_comparisons():
     yield le, Tu(Num, FP), Tu(Num, FP)
     yield ge, Tu(Num, FP), Tu(Num, FP)
     yield eq, Tu(Num, FP), Tu(Num, FP)
@@ -36,7 +47,22 @@ def test_tuple():
     yield lt, Tu(FP, FP), Tu(Num, Num)
 
 
-def test_tuple_varargs():
+def test_varargs_properties():
+    yield eq, \
+          Tu(Num, Num, [Num]).expand_varargs_to(Tu(Num)), \
+          (Type(Num), Type(Num))
+    yield eq, \
+          Tu(Num, [Num]).expand_varargs_to(Tu(Num, Num, Num)), \
+          (Type(Num), Type(Num), Type(Num))
+    yield eq, Tu(Num).expand_varargs_to(Tu(Num, Num)), (Type(Num),)
+    yield eq, Tu(Num, [FP]).base, (Type(Num),)
+    yield ok, Tu([Num]).has_varargs()
+    yield ok, not Tu(Num).has_varargs()
+    yield eq, Tu([Num]).varargs_type, Type(Num)
+    yield raises, RuntimeError, lambda: Tu(Num).varargs_type
+
+
+def test_varargs_comparisons():
     yield lt, Tu(Num, [Num]), Tu([object])
     yield gt, Tu(Num, [Num]), Tu(Num)
     yield lt, Tu(FP, [Num]), Tu(Num, [Num])
