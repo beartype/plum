68539c653bb46da985d96a9edd101092b9210d64
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-04-27 22:37:30 +0100

    Remove Python version check

--- plum/__init__.py
+++ plum/__init__.py
@@ -2,12 +2,6 @@
 
 from __future__ import absolute_import, division, print_function
 
-import sys
-
-vi = sys.version_info
-if vi.major == 3 and vi.minor < 6:  # pragma: no cover
-    raise SystemError('For Python 3, Plum requires at least Python 3.6.')
-
 from .dispatcher import *
 from .function import *
 from .tuple import *
