bf28712cca93626a5cdcf90f779dc8ac372a2bb6
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-05-16 21:25:54 +0100

    Add Cython compilation to speed up Function.__call__

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from __future__ import absolute_import, division, print_function
 
-from setuptools import find_packages, setup
+from setuptools import find_packages, setup, Extension
 
 with open('README.md') as f:
     readme = f.read()
@@ -19,5 +19,6 @@ setup(
     author_email='wessel.p.bruinsma@gmail.com',
     url='https://github.com/wesselb/plum',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
-)
+    packages=find_packages(exclude=('tests', 'docs')),
+    ext_modules=[Extension('plum.function',
+                           ['plum/function.py'])])
