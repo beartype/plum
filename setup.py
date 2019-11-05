7e07e7c668f0d7971b90826d4fcc3a882df98e8c
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-11-05 22:47:23 +0000

    Increment minor version and move to Python 3

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ from setuptools import find_packages, setup, Extension
 requirements = ['numpy>=1.16', 'six']
 
 setup(packages=find_packages(exclude=['docs']),
+      python_requires=['>=3.6'],
       install_requires=requirements,
       ext_modules=[Extension('plum.function',
                              ['plum/function.py'])],
       include_package_data=True)
-
