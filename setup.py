fdbe241055bdbab45226a796fe2a16d0a8779c5b
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-11-05 23:03:34 +0000

    Fix setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup, Extension
 requirements = ['numpy>=1.16', 'six']
 
 setup(packages=find_packages(exclude=['docs']),
-      python_requires=['>=3.6'],
+      python_requires='>=3.6',
       install_requires=requirements,
       ext_modules=[Extension('plum.function',
                              ['plum/function.py'])],
