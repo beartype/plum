343a5e11eaae1c73719e77803228a6b5f4a11497
Cloves Almeida <almeida.cloves@bcg.com>
2021-02-25 23:13:38 -0300

    remove unused dependencies

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
 from setuptools import find_packages, setup, Extension
 
-requirements = ["numpy>=1.16", "six"]
 
 setup(
     packages=find_packages(exclude=["docs"]),
     python_requires=">=3.6",
-    install_requires=requirements,
-    ext_modules=[Extension("plum.function", ["plum/function.py"])],
+    install_requires=[],
     include_package_data=True,
 )
