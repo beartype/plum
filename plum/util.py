e0e24b5e0c39753b235e12ef1a9b3fb862548b08
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2021-04-06 08:15:12 +0200

    Move functions to util

--- plum/util.py
+++ plum/util.py
@@ -1,7 +1,7 @@
 import abc
 import logging
 
-__all__ = ["multihash", "Comparable"]
+__all__ = ["multihash", "Comparable", "is_in_class", "get_class", "get_context"]
 
 log = logging.getLogger(__name__)
 
@@ -57,3 +57,57 @@ class Comparable:
                 otherwise.
         """
         return self < other or self == other or self > other
+
+
+def is_in_class(f):
+    """Check if a function is part of a class.
+
+    Args:
+        f (function): Function to check.
+
+    Returns:
+        bool: `True` if `f` is part of a class, else `False`.
+    """
+    parts = f.__qualname__.split(".")
+    return len(parts) >= 2 and parts[-2] != "<locals>"
+
+
+def _split_parts(f):
+    qualified_name = f.__module__ + "." + f.__qualname__
+    return qualified_name.split(".")
+
+
+def get_class(f):
+    """Assuming that `f` is part of a class, get the fully qualified name of the
+    class.
+
+    Args:
+        f (function): Method to get class name for.
+
+    Returns:
+        str: Fully qualified name of class.
+    """
+    parts = _split_parts(f)
+    return ".".join(parts[:-1])
+
+
+def get_context(f):
+    """Get the fully qualified name of the context for `f`.
+
+    If `f` is part of a class, then the context corresponds to the scope of the class.
+    If `f` is not part of a class, then the context corresponds to the scope of the
+    function.
+
+    Args:
+        f (function): Method to get context for.
+
+    Returns:
+        str: Context.
+    """
+    parts = _split_parts(f)
+    if is_in_class(f):
+        # Split off function name and class.
+        return ".".join(parts[:-2])
+    else:
+        # Split off function name only.
+        return ".".join(parts[:-1])
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from plum.util import multihash, Comparable
+from plum.util import multihash, Comparable, is_in_class, get_class, get_context
 
 
 def test_multihash():
@@ -28,3 +28,31 @@ def test_comparable():
     assert Number(2) > Number(1)
     assert Number(1).is_comparable(Number(2))
     assert not Number(1).is_comparable(Number(np.nan))
+
+
+class A:
+    def f(self):
+        pass
+
+
+def f(self):
+    pass
+
+
+def test_is_in_class():
+    assert is_in_class(A.f)
+    assert is_in_class(A().f)
+    assert not is_in_class(f)
+    assert not is_in_class(lambda _: None)
+
+
+def test_get_class():
+    assert get_class(A.f) == "tests.test_util.A"
+    assert get_class(A().f) == "tests.test_util.A"
+
+
+def test_get_context():
+    assert get_context(A.f) == "tests.test_util"
+    assert get_context(A().f) == "tests.test_util"
+    assert get_context(f) == "tests.test_util"
+    assert get_context(lambda _: None) == "tests.test_util.test_get_context.<locals>"
