27308f7ae652534e15098e68636486e577bd5532
Wessel <wessel.p.bruinsma@gmail.com>
2022-10-21 08:28:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 requirements = []
 
 setup(
-    packages=find_packages(exclude=["docs", "tests"]),
+    packages=find_packages(exclude=["docs", "tests", "tests.*"]),
     python_requires=">=3.7",
     install_requires=requirements,
     include_package_data=True,
