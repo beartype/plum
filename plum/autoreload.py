caf9a5130bfe3b4d0eec3cff441b0b53692096a5
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2023-01-15 20:14:20 +0100

    Minor improvements

--- plum/autoreload.py
+++ plum/autoreload.py
@@ -1,7 +1,7 @@
 import os
 
 from .dispatcher import clear_all_cache
-from .type import _type_mapping
+from .type import type_mapping
 
 __all__ = ["activate", "deactivate"]
 
@@ -16,14 +16,14 @@ def _update_instances(old, new):
     """
     _update_instances_original(old, new)
 
-    _type_mapping[old] = new
+    type_mapping[old] = new
 
     # There might be an existing value in `_type_mapping` that should be replaced by
     # `new`. Resolve these chains.
-    for k, v in _type_mapping.items():
-        while v in _type_mapping:
-            v = _type_mapping[v]
-        _type_mapping[k] = v
+    for k, v in type_mapping.items():
+        while v in type_mapping:
+            v = type_mapping[v]
+        type_mapping[k] = v
 
     # Since types have changed, clear the cache of everything.
     clear_all_cache()
@@ -36,7 +36,7 @@ _update_instances_original = None
 def activate():
     """Pirate Autoreload's `update_instance` function to have Plum work with
     Autoreload."""
-    from IPython.extensions import autoreload
+    from IPython.extensions import autoreload  # type: ignore
 
     # First, cache the original method so we can deactivate ourselves.
     global _update_instances_original
@@ -44,7 +44,7 @@ def activate():
         _update_instances_original = autoreload.update_instances
 
     # Then, override :func:`update_instance`.
-    setattr(autoreload, "update_instances", _update_instances)
+    autoreload.update_instances = _update_instances
 
 
 def deactivate():
@@ -55,7 +55,7 @@ def deactivate():
 
     from IPython.extensions import autoreload
 
-    setattr(autoreload, "update_instances", _update_instances_original)
+    autoreload.update_instances = _update_instances_original
 
 
 _autoload = os.environ.get("PLUM_AUTORELOAD", "0").lower()
@@ -65,7 +65,7 @@ if _autoload in ("y", "yes", "t", "true", "on", "1"):  # pragma: no cover
     try:
         # Try to load iPython and get the iPython session, but don't crash if
         # this does not work (for example IPython not installed, or python shell)
-        from IPython import get_ipython
+        from IPython import get_ipython  # type: ignore
 
         ip = get_ipython()
         if ip is not None:
