16e23fd3565208ed6e44bd26d9d7bd7cf70dc86d
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2023-01-21 14:39:46 +0100

    Improve style

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,19 +1,20 @@
-import typing
+from typing import Union
 
 import plum
+from plum import ModuleType
 
 
 def test_isinstance():
     # Check that subscripted generics work and types are resolved.
     assert plum.isinstance(
         1,
-        typing.Union[float, plum.ModuleType("builtins", "int")],  # noqa: F821
+        Union[float, ModuleType("builtins", "int")],  # noqa: F821
     )
 
 
 def test_issubclass():
     # Check that subscripted generics work and types are resolved.
     assert plum.issubclass(
-        typing.Union[plum.ModuleType("builtins", "int"), float],  # noqa: F821
-        typing.Union[plum.ModuleType("numbers", "Number"), complex],  # noqa: F821
+        Union[ModuleType("builtins", "int"), float],  # noqa: F821
+        Union[ModuleType("numbers", "Number"), complex],  # noqa: F821
     )
