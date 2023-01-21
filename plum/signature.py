17009a5e5b4a838145129f993cb2c945cd927100
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2023-01-21 14:31:26 +0100

    Rework and test `signature.py`

--- plum/signature.py
+++ plum/signature.py
@@ -1,112 +1,295 @@
-import logging
+import inspect
+import operator
+import typing
 
-from .type import ptype, VarArgs
-from .util import Comparable, multihash
+from beartype.door import TypeHint, is_bearable
+from beartype.peps import resolve_pep563
 
-__all__ = ["Signature"]
+from .type import is_faithful, resolve_type_hint
+from .util import Comparable, Missing, multihash, wrap_lambda
 
-log = logging.getLogger(__name__)
+__all__ = ["Signature", "extract_signature", "append_default_args"]
 
 
 class Signature(Comparable):
     """Signature.
 
     Args:
-        *types (type or ptype): Types of the arguments.
+        *types (tuple[type, ...]): Types of the arguments.
+        varargs (type, optional): Type of the variable arguments.
+        return_type (type, optional): Type of the return value. Defaults to `Any`.
+        precedence (int, optional): Precedence. Default to `0`.
+        implementation (function, optional): Implementation.
+
+    Attributes:
+        types (tuple[type, ...]): Types of the arguments.
+        varargs (type or :class:`.util.Missing`): Type of the variable number of
+            arguments.
+        has_varargs (bool): Whether `varargs` is not :class:`.util.Missing`.
+        return_type (type): Return type.
+        precedence (int): Precedence.
+        implementation (function or None): Implementation.
+        is_faithful (bool): Whether this signature only uses faithful types.
     """
 
-    def __init__(self, *types):
-        # Ensure that all types are Plum types.
-        self.types = tuple(ptype(t) for t in types)
+    def __init__(
+        self,
+        *types,
+        varargs=Missing,
+        return_type=typing.Any,
+        precedence=0,
+        implementation=None,
+    ):
+        self.types = types
+        self.varargs = varargs
+        self.has_varargs = varargs is not Missing
+        self.return_type = return_type
+        self.precedence = precedence
+        self.implementation = implementation
 
-        # Ensure that only the last type possibly represents varargs.
-        if any([isinstance(t, VarArgs) for t in self.types[:-1]]):
-            raise TypeError("Only the last type can represent varargs.")
+        types_are_faithful = all(is_faithful(t) for t in types)
+        varargs_are_faithful = self.varargs is Missing or is_faithful(self.varargs)
+        self.is_faithful = types_are_faithful and varargs_are_faithful
+
+    def __copy__(self):
+        return Signature(
+            *self.types,
+            varargs=self.varargs,
+            return_type=self.return_type,
+            precedence=self.precedence,
+            implementation=self.implementation,
+        )
 
     def __repr__(self):
-        return "Signature({})".format(", ".join(map(repr, self.types)))
+        types_repr = ", ".join(map(repr, self.types))
+        if types_repr:
+            types_repr += ", "
+        return (
+            f"Signature("
+            f"{types_repr}"
+            f"varargs={self.varargs!r}, "
+            f"return_type={self.return_type!r})"
+        )
 
     def __hash__(self):
-        return multihash(Signature, *self.types)
+        return multihash(Signature, *self.types, self.varargs)
 
-    def __len__(self):
-        return len(self.base)
-
-    def expand_varargs_to(self, other):
-        """Expand varargs to a given :class:`.signature.Signature`.
+    def expand_varargs(self, n):
+        """Expand variable arguments.
 
         Args:
-            other (:class:`.signature.Signature`): Other tuple to expand to.
+            n (int): Desired number of types.
 
         Returns:
-            tuple[ptype]: Tuple of Plum types that matches `other`.
+            tuple[type, ...]: Expanded types.
         """
-        if self.has_varargs():
-            expansion_size = max(len(other) - len(self), 0)
-            types = self.base + self.types[-1].expand(expansion_size)
-            return types
+        if self.has_varargs:
+            expansion_size = max(n - len(self.types), 0)
+            return self.types + (self.varargs,) * expansion_size
         else:
-            return self.base
+            return self.types
 
     def __le__(self, other):
-        # Check varargs.
-        if self.has_varargs() and not other.has_varargs():
+        # If this signature has variable arguments, but the other does not, then this
+        # signature cannot be possibly smaller.
+        if self.has_varargs and not other.has_varargs:
             return False
-        elif (
-            self.has_varargs()
-            and other.has_varargs()
-            and self.varargs_type > other.varargs_type
+
+        # If this signature and the other signature both have variable arguments, then
+        # the variable type of this signature must be less than the variable type of the
+        # other signature.
+        if (
+            self.has_varargs
+            and other.has_varargs
+            and not (TypeHint(self.varargs) <= TypeHint(other.varargs))
         ):
             return False
 
-        # Check compatibility.
-        if not self.is_compatible(other):
+        # If the number of types of the signatures are unequal, then the signature
+        # with the fewer number of types must be expanded using variable arguments.
+        if not (
+            len(self.types) == len(other.types)
+            or (len(self.types) > len(other.types) and other.has_varargs)
+            or (len(self.types) < len(other.types) and self.has_varargs)
+        ):
             return False
 
-        # Finally, compare.
+        # Finally, expand the types and compare.
+        self_types = self.expand_varargs(len(other.types))
+        other_types = other.expand_varargs(len(self.types))
         return all(
-            [
-                x <= y
-                for x, y in zip(
-                    self.expand_varargs_to(other), other.expand_varargs_to(self)
-                )
-            ]
+            [TypeHint(x) <= TypeHint(y) for x, y in zip(self_types, other_types)]
         )
 
-    @property
-    def base(self):
-        """Base of the tuple."""
-        return self.types[:-1] if self.has_varargs() else self.types
-
-    def has_varargs(self):
-        """Check whether this signature has varargs.
-
-        Returns:
-            bool: `True` if and only if this signature has varargs.
-        """
-        return len(self.types) > 0 and isinstance(self.types[-1], VarArgs)
-
-    @property
-    def varargs_type(self):
-        """Type of the varargs."""
-        if not self.has_varargs():
-            raise RuntimeError(
-                "Type of varargs requested, but tuple does not have varargs."
-            )
-        return self.types[-1].type
-
-    def is_compatible(self, other):
-        """Check whether this signature is compatible with another one.
+    def match(self, values):
+        """Check whether values match the signature.
 
         Args:
-            other (:class:`.signature.Signature`): Other signature to check
-                compatibility with.
+            values (tuple): Values.
 
         Returns:
-            bool: Compatibility.
+            bool: `True` if `values` match this signature and `False` otherwise.
         """
-        return (
-            len(self) == len(other)
-            or (len(self) > len(other) and other.has_varargs())
-            or (len(self) < len(other) and self.has_varargs())
-        )
+        # `values` must either be exactly many as `self.types`. If there are more
+        # `values`, then there must be variable arguments to cover the arguments.
+        if not (
+            len(self.types) == len(values)
+            or (len(self.types) < len(values) and self.has_varargs)
+        ):
+            return False
+        else:
+            types = self.expand_varargs(len(values))
+            return all(is_bearable(v, t) for v, t in zip(values, types))
+
+
+def _is_not_empty(t):
+    """Check if a type is *not* equal to `inspect.Parameter.empty`, without triggering
+    unwanted `__eq__`-like methods.
+
+    Args:
+        t (type): Type to check.
+
+    Returns:
+        bool: `True` if `t` is *not* `inspect.Parameter.empty`.
+    """
+    return t is not inspect.Parameter.empty
+
+
+def _inspect_signature(f):
+    """Wrapper of :func:`inspect.signature` which adds support for certain non-function
+    objects.
+
+    Args:
+        f (object): Function-like object.
+
+    Returns:
+        object: Signature.
+    """
+    if isinstance(f, operator.itemgetter):
+        f = wrap_lambda(f)
+    elif isinstance(f, operator.attrgetter):
+        f = wrap_lambda(f)
+    return inspect.signature(f)
+
+
+def extract_signature(f, precedence=0):
+    """Extract the signature from a function.
+
+    Args:
+        f (function): Function to extract signature from.
+        precedence (int, optional): Precedence of the method.
+
+    Returns:
+        :class:`.Signature`: Signature.
+    """
+    if hasattr(f, "__annotations__"):
+        resolve_pep563(f)  # This mutates `f`.
+        # Override the `__annotations__` attribute, since `resolve_pep563` modifies
+        # `f` too.
+        for k, v in typing.get_type_hints(f).items():
+            f.__annotations__[k] = v
+
+    # Extract specification.
+    sig = _inspect_signature(f)
+
+    # Get types of arguments.
+    types = []
+    varargs = Missing
+    for arg in sig.parameters:
+        p = sig.parameters[arg]
+
+        # Parse and resolve annotation.
+        if _is_not_empty(p.annotation):
+            annotation = resolve_type_hint(p.annotation)
+        else:
+            annotation = typing.Any
+
+        # Stop once we have seen all positional parameter without a default value.
+        if p.kind in {p.KEYWORD_ONLY, p.VAR_KEYWORD}:
+            break
+
+        if p.kind == p.VAR_POSITIONAL:
+            # Parameter indicates variable arguments.
+            varargs = annotation
+        else:
+            # Parameter is a regular positional parameter.
+            types.append(annotation)
+
+        # If there is a default parameter, make sure that it is of the annotated type.
+        if _is_not_empty(p.default):
+            if not is_bearable(p.default, annotation):
+                raise TypeError(
+                    f"Default value `{p.default}` is not an instance "
+                    f"of the annotated type `{annotation}`."
+                )
+
+    # Get possible return type.
+    if _is_not_empty(sig.return_annotation):
+        return_type = resolve_type_hint(sig.return_annotation)
+    else:
+        return_type = typing.Any
+
+    # Assemble signature.
+    signature = Signature(
+        *types,
+        varargs=varargs,
+        return_type=return_type,
+        precedence=precedence,
+        implementation=f,
+    )
+
+    return signature
+
+
+def append_default_args(signature, f):
+    """Returns a list of signatures of function `f`, where those signatures are derived
+    from the input arguments of `f` by treating every non-keyword-only argument with a
+    default value as a keyword-only argument turn by turn.
+
+    Args:
+        f (function): Function to extract default arguments from.
+        signature (:class:`.signature.Signature`): Signature of `f` from which to
+            remove default arguments.
+
+    Returns:
+        list[:class:`.signature.Signature`]: list of signatures excluding from 0 to all
+        default arguments.
+    """
+    # Extract specification.
+    f_signature = _inspect_signature(f)
+
+    signatures = [signature]
+
+    arg_names = list(f_signature.parameters.keys())
+    # We start at the end and, once we reach non-keyword-only arguments, delete the
+    # argument with defaults values one by one. This generates a sequence of signatures,
+    # which we return.
+    arg_names.reverse()
+
+    for arg in arg_names:
+        p = f_signature.parameters[arg]
+
+        # Ignore variable arguments and keyword arguments.
+        if p.kind in {p.VAR_KEYWORD, p.KEYWORD_ONLY}:
+            continue
+
+        # Stop when non-variable arguments without a default are reached.
+        if p.kind != p.VAR_POSITIONAL and not _is_not_empty(p.default):
+            break
+
+        # Copy last signature.
+        copy = signatures[-1].__copy__()
+
+        # Remove variable arguments.
+        if p.kind == p.VAR_POSITIONAL:
+            # We could skip this method entirely, but the implementation is simpler if
+            # we retain it.
+            copy.varargs = Missing
+
+        # Remove a normal positional argument.
+        else:
+            copy.types = copy.types[:-1]
+
+        signatures.append(copy)
+
+    return signatures
--- tests/test_signature.py
+++ tests/test_signature.py
@@ -1,95 +1,219 @@
-import pytest
+import inspect
+import operator
 import sys
+from numbers import Number as Num
+from numbers import Rational as Rat
+from numbers import Real as Re
+from typing import Any, Tuple, Union
+
+import pytest
 
 from plum.signature import Signature as Sig
-from plum.type import Type, VarArgs, Union
+from plum.signature import (
+    _inspect_signature,
+    _is_not_empty,
+    append_default_args,
+    extract_signature,
+)
+from plum.util import Missing
 
 
-def test_instantiation():
-    with pytest.raises(TypeError):
-        Sig(VarArgs(int), int)
-    with pytest.raises(TypeError):
-        Sig(VarArgs(int, str))
+def test_instantiation_copy():
+    s = Sig(
+        int,
+        int,
+        varargs=float,
+        return_type=complex,
+        precedence=1,
+        implementation=lambda *xs: complex(sum(xs)),
+    )
+    for _ in range(2):
+        assert s.types == (int, int)
+        assert s.has_varargs
+        assert s.varargs == float
+        assert s.return_type == complex
+        assert s.precedence == 1
+        assert s.implementation(1, 2, 3.0) == 6 + 0j
+        assert s.is_faithful
+
+        # Test copying.
+        s = s.__copy__()
+
+    # Test defaults.
+    s = Sig(int, int)
+    assert not s.has_varargs
+    assert s.varargs == Missing
+    assert s.return_type == Any
+    assert s.implementation is None
+
+    # Test faithfulness check.
+    assert Sig(int, int).is_faithful
+    assert Sig(int, int, varargs=int).is_faithful
+    assert not Sig(Tuple[int], int, varargs=int).is_faithful
+    assert not Sig(int, Tuple[int], varargs=int).is_faithful
+    assert not Sig(int, int, varargs=Tuple[int]).is_faithful
 
 
-class Num:
-    pass
+@pytest.mark.skipif(
+    sys.version_info < (3, 10),
+    reason="Requires Python 3.10 or higher.",
+)
+def test_instantiation_new_union_syntax():
+    assert Sig(float | int) == Sig(Union[float, int])
+    assert Sig(Num | Rat) == Sig(Union[Num, Rat])
 
 
-class Re(Num):
-    pass
+@pytest.mark.parametrize(
+    "sig, expected",
+    [
+        (
+            Sig(),
+            f"Signature(varargs={Missing!r}, return_type={Any!r})",
+        ),
+        (
+            Sig(int),
+            f"Signature({int!r}, varargs={Missing!r}, return_type={Any!r})",
+        ),
+        (
+            Sig(int, float),
+            f"Signature({int!r}, {float!r}, varargs={Missing!r}, return_type={Any!r})",
+        ),
+        (
+            Sig(varargs=int, return_type=float),
+            f"Signature(varargs={int!r}, return_type={float!r})",
+        ),
+    ],
+)
+def test_repr(sig, expected):
+    assert repr(sig) == expected
 
 
-class FP(Re):
-    pass
+def test_hash():
+    assert hash(Sig(int)) == hash(Sig(int))
+    sigs = {Sig(int), Sig(int, int), Sig(int, int, varargs=int)}
+    assert len(sigs) == 3
 
 
-def test_properties():
-    assert hash(Sig(Num)) == hash(Sig(Num))
-    assert hash(Sig(Num)) != hash(Type(Num))
-    assert len(Sig(Num)) == 1
-    assert len(Sig(Num, VarArgs(Num))) == 1
+def test_expand_varargs():
+    # Case of no variable arguments:
+    assert Sig(int, int).expand_varargs(3) == (int, int)
+
+    # Case of variable arguments:
+    s = Sig(int, int, varargs=float)
+    assert s.expand_varargs(2) == (int, int)
+    assert s.expand_varargs(3) == (int, int, float)
+    assert s.expand_varargs(4) == (int, int, float, float)
 
 
-def test_representation():
-    assert repr(Sig()) == "Signature()"
-    assert repr(Sig(Num, Re)) == f"Signature({Type(Num)!r}, {Type(Re)!r})"
+def test_comparison():
+    # Variable arguments shortcuts:
+    assert not Sig(varargs=int) <= Sig()
+    assert not Sig(varargs=Num) <= Sig(varargs=int)
+
+    # Expandability shortcuts:
+    assert not Sig(int) <= Sig(int, int)
+    assert not Sig(int) <= Sig(int, int, varargs=int)
+    assert not Sig(int, int, varargs=int) <= Sig(int)
+
+    # Test expansion:
+    assert Sig(varargs=int) <= Sig(Re, varargs=Re)
+    assert Sig(int, varargs=int) <= Sig(Re, varargs=Re)
+    assert Sig(int, int, varargs=int) <= Sig(Re, varargs=Re)
+
+    assert not Sig(varargs=Num) <= Sig(Re, varargs=Re)
+    assert not Sig(Num, varargs=int) <= Sig(Re, varargs=Re)
+    assert not Sig(int, int, varargs=Num) <= Sig(Re, varargs=Re)
+
+    assert Sig(float, varargs=int) <= Sig(float, Re, varargs=Re)
+    assert Sig(float, int, varargs=int) <= Sig(float, Re, varargs=Re)
+    assert Sig(float, int, int, varargs=int) <= Sig(float, Re, varargs=Re)
 
 
-def test_comparability():
-    assert Sig(Num).is_comparable(Sig(FP))
-    assert not Sig(Num).is_comparable(Sig(int))
+def test_match():
+    assert Sig(int).match((1,))
+    assert Sig(int, int).match((1, 2))
+    assert Sig(int, varargs=int).match((1,))
+    assert Sig(int, varargs=int).match((1, 2))
+    assert Sig(int, varargs=int).match((1, 2, 3))
+
+    # Wrong type:
+    assert not Sig(int).match((1.0,))
+    assert not Sig(int, int).match((1, 2.0))
+    assert not Sig(int, varargs=int).match((1.0,))
+    assert not Sig(int, varargs=int).match((1, 2.0))
+    assert not Sig(int, varargs=int).match((1, 2, 3.0))
+
+    # Wrong number:
+    assert not Sig(int).match((1, 2))
+    assert not Sig(int, int).match((1,))
+    assert not Sig(int, varargs=int).match(())
 
 
-def test_comparisons():
-    assert Sig(Num, FP) <= Sig(Num, FP)
-    assert Sig(Num, FP) >= Sig(Num, FP)
-    assert Sig(Num, FP) == Sig(Num, FP)
-    assert Sig(Num, Re) < Sig(Num, Num)
-    assert Sig(Num, Num) > Sig(Num, Re)
-    assert Sig(Num, Re) < Sig(Num, Num)
-    assert Sig(FP, Num) < Sig(Num, Num)
-    assert Sig(FP, FP) < Sig(Num, Num)
+def test_is_not_empty():
+    assert not _is_not_empty(inspect.Parameter.empty)
+    assert _is_not_empty(None)
 
 
-def test_varargs_properties():
-    expanded_sig = Sig(Num, Num, VarArgs(Num)).expand_varargs_to(Sig(Num))
-    assert expanded_sig == (Type(Num), Type(Num))
-    expanded_sig = Sig(Num, VarArgs(Num)).expand_varargs_to(Sig(Num, Num, Num))
-    assert expanded_sig == (Type(Num), Type(Num), Type(Num))
-    assert Sig(Num).expand_varargs_to(Sig(Num, Num)) == (Type(Num),)
-    assert Sig(Num, VarArgs(FP)).base == (Type(Num),)
-    assert Sig(VarArgs(Num)).has_varargs()
-    assert not Sig(Num).has_varargs()
-    assert Sig(VarArgs(Num)).varargs_type == Type(Num)
-    with pytest.raises(RuntimeError):
-        Sig(Num).varargs_type
+def test_inspect_signature():
+    assert isinstance(_inspect_signature(lambda x: x), inspect.Signature)
+    assert len(_inspect_signature(lambda x: x).parameters) == 1
+    assert len(_inspect_signature(operator.itemgetter(1)).parameters) == 1
+    assert len(_inspect_signature(operator.attrgetter("x")).parameters) == 1
 
 
-def test_varargs_comparisons():
-    assert Sig(Num, VarArgs(Num)) < Sig(VarArgs(object))
-    assert Sig(Num, VarArgs(Num)) > Sig(Num)
-    assert Sig(FP, VarArgs(Num)) < Sig(Num, VarArgs(Num))
-    assert Sig(FP, VarArgs(FP)) < Sig(FP, VarArgs(Num))
-    assert Sig(FP, VarArgs(FP)) <= Sig(FP, VarArgs(Num))
-    assert Sig(FP, VarArgs(FP)) != Sig(FP, VarArgs(Num))
-    assert Sig(FP, Num) < Sig(FP, Num, VarArgs(Num))
-    assert Sig(Num, Num) < Sig(Num, VarArgs(Num))
-    assert not Sig(Num, Num).is_comparable(Sig(FP, VarArgs(FP)))
-    assert not Sig(Num, VarArgs(FP)).is_comparable(Sig(Num, Num))
-    assert Sig(VarArgs(Num)) == Sig(Num, VarArgs(Num))
-    assert Sig(VarArgs(Num)) > Sig(Re, VarArgs(Num))
-    assert Sig(Num, VarArgs(Num)) == Sig(Num, Num, Num, VarArgs(Num))
+def assert_signature(f, *types, varargs=Missing, return_type=Any):
+    sig = extract_signature(f)
+    assert sig.types == types
+    assert sig.varargs == varargs
+    assert sig.return_type == return_type
 
 
-def test_union():
-    assert Sig(Union(Num, Num)) == Sig(Union(Num))
-    assert Sig(Union(FP, Num)) == Sig(Union(Num))
-    assert Sig(Union(FP, Num)) >= Sig(Union(Re))
-    assert Sig(Union(FP)) < Sig(Union(Num, Re))
+def test_extract_signature():
+    def f():
+        pass
+
+    # Check implementation and precedence.
+    assert extract_signature(f).implementation == f
+    assert extract_signature(f).precedence == 0
+    assert extract_signature(f, precedence=1).precedence == 1
+
+    # Check defaults.
+    assert_signature(f)
+
+    # Check a more complex example.
+
+    def f(a: int, b, *c: float, **kw_args: Num) -> Re:
+        pass
+
+    assert_signature(f, int, Any, varargs=float, return_type=Re)
+
+    # Check that default values must be right.
+
+    def f_good(a: int = 1):
+        pass
+
+    def f_bad(a: int = 1.0):
+        pass
+
+    assert_signature(f_good, int)
+    with pytest.raises(
+        TypeError,
+        match=r"Default value `.*` is not an instance of the annotated type `.*`.",
+    ):
+        extract_signature(f_bad)
 
 
-@pytest.mark.skipif(sys.version_info < (3, 10), reason="requires python3.10 or higher")
-def test_union_new_syntax():
-    assert Sig(eval("float | int")) == Sig(Union(float, int))
-    assert Sig(eval("Num | FP")) == Sig(Union(Num, FP))
+def test_append_default_args():
+    def f(a: int, b=1, c: float = 1.0, *d: complex, option=None, **other_options):
+        pass
+
+    sigs = append_default_args(extract_signature(f), f)
+    assert len(sigs) == 4
+    assert (sigs[0].types, sigs[0].varargs) == ((int, Any, float), complex)
+    assert (sigs[1].types, sigs[1].varargs) == ((int, Any, float), Missing)
+    assert (sigs[2].types, sigs[2].varargs) == ((int, Any), Missing)
+    assert (sigs[3].types, sigs[3].varargs) == ((int,), Missing)
+
+    # Test that `itemgetter` is supported.
+    f = operator.itemgetter(0)
+    assert len(append_default_args(extract_signature(f), f)) == 1
