170687b933ae5e29eefac95fc1e5653b75173777
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2023-02-19 15:48:07 +0100

    Improve `benchmark`

--- tests/util.py
+++ tests/util.py
@@ -1,17 +1,29 @@
 from time import time
 
 
-def benchmark(f, args, n=1000):
+def benchmark(f, args, n=1000, burn=0, setup=lambda: None):
     """Benchmark the performance of a function `f` called with arguments
     `args` in microseconds.
 
     Args:
         f (function): Function to benchmark.
         args (tuple): Argument to call `f` with.
-        n (int): Repetitions.
+        n (int, optional): Repetitions. Defaults to 1000.
+        burn (int, optional): Perform so many repetitions before timing.
+        setup (function, optional): Call this function before every call to `f`.
+
+    Returns:
+        float: Average execution time in milliseconds.
     """
-    start = time()
-    for i in range(n):
+    # Perform burn-in.
+    for _ in range(burn):
+        setup()
         f(*args)
-    dur = time() - start
-    return dur * 1e6 / n
+    # Perform timing.
+    durations = []
+    for _ in range(n):
+        setup()
+        start = time()
+        f(*args)
+        durations.append(time() - start)
+    return sum(durations) * 1e6 / n
