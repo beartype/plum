87fec4fa334496ceabb88816cfddf2b2f2658386
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2023-08-15 19:32:21 +0200

    Import `Any`

--- plum/overload.py
+++ plum/overload.py
@@ -1,5 +1,5 @@
 import sys
-from typing import Callable, TypeVar
+from typing import Any, Callable, TypeVar
 
 if sys.version_info >= (3, 11):  # pragma: specific no cover 3.7 3.8 3.9 3.10
     from typing import get_overloads, overload
--- plum/overload.pyi
+++ plum/overload.pyi
@@ -1,9 +1,9 @@
-from typing import Callable, TypeVar, overload
+from typing import Any, Callable, TypeVar, overload
 
 from plum import Function
 
 __all__ = ["overload", "dispatch"]
 
-T = TypeVar("T", bound=Callable)
+T = TypeVar("T", bound=Callable[..., Any])
 
 def dispatch(f: Callable) -> Function: ...
