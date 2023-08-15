94e35ced148a278db57c516dd98b32a67312e1c4
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2023-08-15 18:46:37 +0200

    Fix type stub

--- plum/overload.pyi
+++ plum/overload.pyi
@@ -1,7 +1,9 @@
 from typing import Callable, TypeVar, overload
 
+from plum import Function
+
 __all__ = ["overload", "dispatch"]
 
 T = TypeVar("T", bound=Callable)
 
-def dispatch(f: T) -> T: ...
+def dispatch(f: Callable) -> Function: ...
