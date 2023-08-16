71bbe24037c75e0d946e44e80e3ee722cf670964
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2023-08-16 19:08:38 +0200

    Delete unnecessary `overload.pyi`

--- plum/overload.pyi
+++
@@ -1,9 +0,0 @@
-from typing import Any, Callable, TypeVar, overload
-
-from plum import Function
-
-__all__ = ["overload", "dispatch"]
-
-T = TypeVar("T", bound=Callable[..., Any])
-
-def dispatch(f: Callable) -> Function: ...
