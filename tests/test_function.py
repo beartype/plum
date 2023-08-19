c10fc4732b335b3f2b982375c445284abe059de0
Gabriel de Marmiesse <gabrieldemarmiesse@gmail.com>
2023-08-19 19:10:01 +0200

    :wrench: Group logic concerning the cache in one single method (#96)
    * :wrench: Group logic concerning the cache in one single method
    
    * Add small unit test

--- tests/test_function.py
+++ tests/test_function.py
@@ -87,6 +87,16 @@ def test_owner():
     assert Function(f, owner="A").owner is A
 
 
+def test_resolve_method_with_cache_no_arguments():
+    def f(x):
+        pass
+
+    with pytest.raises(ValueError) as err:
+        Function(f)._resolve_method_with_cache()
+
+    assert "args and types cannot be None" in str(err)
+
+
 @pytest.fixture()
 def owner_transfer():
     # Save and clear.
