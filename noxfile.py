22985fd46616e7abae72f1e25dfc23fd47b14d3f
nstarman <nstarman@users.noreply.github.com>
2025-12-03 14:01:26 -0500

    ci: overhual tox to nox
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

---
+++ noxfile.py
@@ -0,0 +1,56 @@
+"""Nox setup."""
+
+import sys
+from pathlib import Path
+
+import nox
+from nox_uv import session
+
+nox.needs_version = ">=2024.3.2"
+nox.options.default_venv_backend = "uv"
+
+DIR = Path(__file__).parent.resolve()
+
+# =============================================================================
+# Linting
+
+
+@session(uv_groups=["lint"], reuse_venv=True)
+def lint(s: nox.Session, /) -> None:
+    """Run the linter."""
+    precommit(s)  # reuse pre-commit session
+    pylint(s)  # reuse pylint session
+
+
+@session(uv_groups=["lint"], reuse_venv=True)
+def precommit(s: nox.Session, /) -> None:
+    """Run pre-commit."""
+    s.run("pre-commit", "run", "--all-files", *s.posargs)
+
+
+@session(uv_groups=["lint"], reuse_venv=True)
+def pylint(s: nox.Session, /) -> None:
+    """Run PyLint."""
+    s.install(".", "pylint")
+    s.run("pylint", "plum", *s.posargs)
+
+
+# =============================================================================
+# Testing
+
+
+@session(uv_groups=["test"], reuse_venv=True)
+def typecheck(s: nox.Session, /) -> None:
+    """Run the type checker."""
+    s.run("python", "check_linter_assertions.py", "tests/typechecked", *s.posargs)
+
+
+@session(uv_groups=["test"], reuse_venv=True)
+def pytest(s: nox.Session, /) -> None:
+    """Run the unit and regular tests."""
+    # Compute from the Python in this nox/uv environment
+    pragma_version = ".".join(map(str, sys.version_info[:2]))
+    s.env["PRAGMA_VERSION"] = pragma_version
+
+    # Run pytest
+    s.run("pytest", *s.posargs)
