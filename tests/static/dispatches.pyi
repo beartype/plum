78fc7c9a3b868e917406cddcbebec1a4025173db
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2025-12-12 17:39:18 +0100

    Make error documentation consistent between files

--- tests/static/dispatches.pyi
+++ tests/static/dispatches.pyi
@@ -2,27 +2,27 @@ from typing_extensions import assert_type
 
 import plum
 
-# Pyright errors:
+# PyRight errors:
 #   - Function declaration "f" is obscured by a declaration of the same name
 @plum.dispatch
 def f(x: int) -> str:  # pyright: ignore[reportRedeclaration]
     return str(x)
 
-# Mypy errors:
+# MyPy errors:
 #   - Cannot infer return type (`Expression is of type "Any", not "str"`)
 assert_type(f(1), str)  # type: ignore[assert-type]
 
-# Mypy errors:
+# MyPy errors:
 #   - Name "f" already defined on line ...
 @plum.dispatch  # type: ignore[no-redef]
 def f(x: str) -> str:
     return x
 
-# Mypy errors:
+# MyPy errors:
 #   - Cannot infer return type (`Expression is of type "Any", not "str"`)
 assert_type(f(1), str)  # type: ignore[assert-type]
 
-# Mypy errors:
+# MyPy errors:
 #   - Argument 1 to "f" has incompatible type "str"; expected "int"
 #   - Cannot infer return type (`Expression is of type "Any", not "str"`)
 assert_type(f("1"), str)  # type: ignore[arg-type,assert-type]
--- tests/static/overload.pyi
+++ tests/static/overload.pyi
@@ -8,19 +8,20 @@ def f(x: int) -> int:
 def f(x: str) -> str:
     return x
 
-@plum.dispatch  # type: ignore[misc]  # "An implementation for an overloaded function is not allowed in a stub file"
+# MyPy errors:
+# - An implementation for an overloaded function is not allowed in a stub file
+@plum.dispatch  # type: ignore[misc]
 def f(x):
     pass
 
 assert f(1) == 1
 assert f("1") == "1"
 
-# The following should raise the following errors:
-# - pyright
+# - MyPy errors:
+#   - call-overload: No overload variant of "f" matches argument type "float"
+# - PyRight errors:
 #   - reportArgumentType: Argument of type "float" cannot be assigned to
 #     parameter "x" of type "str" in function "f" "float" is not assignable to
 #     "str"
 #   - reportCallIssue: No overloads for "f" match the provided arguments
-# - mypy:
-#   - call-overload: No overload variant of "f" matches argument type "float"
 f(1.0)  # type: ignore[call-overload]  # pyright: ignore[reportArgumentType,reportCallIssue]
